13fc71aa5cb6a447e993bcca097628ce4d4cdc6b
Александра  Дырда <127667822+AlexandraDyrda@users.noreply.github.com>
2023-04-11 14:20:55 +0000

    I have changed the size of the array from 7 up to 10

--- .ccls-cache/@home@runner@opiselfwork4/main.cpp
+++ .ccls-cache/@home@runner@opiselfwork4/main.cpp
@@ -1,5 +1,40 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+//!Ввести два масиви та створити третій зі спільних елементів масивів; Розмір масиву до 10; Тип даних дійсний.
 
 int main() {
-  std::cout << "Hello Self-Work_4!\n";
-}
+    srand(time(NULL));
+    int n;
+    double first_array[10], second_array[10], third_array[10]; 
+    AGAIN:
+    cout << "Enter the size of the array (up to 10): " << " "; cin >> n;
+    if (n > 10) {
+        cout << "Try again!" << endl;
+        goto AGAIN;
+    }
+    cout << "The first random array of " << n << " elements:" << endl; 
+    for (int i = 0; i < n; i++) {
+       first_array[i] = rand() % 20;
+       cout << first_array[i] << " ";
+    }
+    cout << "\n" << "The second random array of " << n << " elements:" << endl; 
+    for (int i = 0; i < n; i++) {
+       second_array[i] = rand() % 20;
+       cout << second_array[i] << " ";
+    }
+    cout << "\n" << "The third array of common elements:"<< endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if(first_array[i] == second_array[j]) {
+                second_array[j] = second_array[j + 1];
+                j--;
+                third_array[i] = first_array[i];
+                cout << third_array[i] << " ";
+                break;
+            } 
+        }
+    }
+} 
