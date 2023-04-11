4844edb52818d9eaae74d81f922210fda9b25dd3
Александра  Дырда <127667822+AlexandraDyrda@users.noreply.github.com>
2023-04-11 14:52:20 +0000

    A new code

--- .ccls-cache/@home@runner@opiselfwork4/main.cpp
+++ .ccls-cache/@home@runner@opiselfwork4/main.cpp
@@ -3,29 +3,26 @@
 #include <ctime>
 using namespace std;
 
-//!Ввести два масиви та створити третій зі спільних елементів масивів; Розмір масиву до 10; Тип даних дійсний.
+//!Ввести два масиви та створити третій зі спільних елементів масивів; Розмір масиву до 7; Тип даних дійсний.
 
 int main() {
     srand(time(NULL));
     int n;
-    double first_array[10], second_array[10], third_array[10]; 
+    double first_array[7], second_array[7], third_array[7]; 
     AGAIN:
-    cout << "Enter the size of the array (up to 10): " << " "; cin >> n;
-    if (n > 10) {
+    cout << "Enter the size of the array (up to 7): " << " "; cin >> n;
+    if (n > 7) {
         cout << "Try again!" << endl;
-        goto AGAIN;
-    } else if (n == 10) {
-        cout << "It has to be less than 10!" << endl;
         goto AGAIN;
     }
     cout << "The first random array of " << n << " elements:" << endl; 
     for (int i = 0; i < n; i++) {
-       first_array[i] = rand() % 10;
+       first_array[i] = rand() % 20;
        cout << first_array[i] << " ";
     }
     cout << "\n" << "The second random array of " << n << " elements:" << endl; 
     for (int i = 0; i < n; i++) {
-       second_array[i] = rand() % 10;
+       second_array[i] = rand() % 20;
        cout << second_array[i] << " ";
     }
     cout << "\n" << "The third array of common elements:"<< endl;
